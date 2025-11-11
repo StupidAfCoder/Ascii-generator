5aaae907c0c645e85cd6f4e510f6b5d0bb4a6aa1
StupidAfCoder <malikyash2007@gmail.com>
2025-11-12 00:26:04 +0530

    Linear Luminance

--- src/ascii-converter.cpp
+++ src/ascii-converter.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #include <iostream>
@@ -27,17 +28,19 @@ void Ascii::generate(){
     int y = 0;
     int x = 0;
     int index{};
+    std::vector<Pixels> pixel_array;
 
+    //During The Main Program
     /*for (y ; y < height ; y++){
         for (x ; x < width ; x++){
             index = ( y * width + x) * desired_channels;
             int red = pixel_data[index];
             int green = pixel_data[index+1];
             int blue = pixel_data[index+2];
-            std::cout << "( " << red << green << blue << " ) || ";                
         }
     }*/
 
+    //Calculate Index and RGB values For One Pixel!!
     index = (y * width + x) * desired_channels;
 
     int red = pixel_data[index];
@@ -45,4 +48,47 @@ void Ascii::generate(){
     int blue = pixel_data[index+2];
 
     std::cout << "( " << red << green << blue << " )" << std::endl; 
+
+    double rf = red / 255.0;
+    double gf = green / 255.0;
+    double bf = blue / 255.0;
+
+    double r_lin{};
+    double g_lin{};
+    double b_lin{};
+
+    std::cout << "( " << rf << " " << gf << " " << bf << " " << " )" << std::endl;
+
+    //Have linearize for each value!!!! //Not Important but gets more sharp output :) (when sRGB)
+    std::vector<double> values = {rf,gf,bf};
+    for (int i = 0; i < 3 ; i++){
+        if (values[i] <= 0.04045) {
+            if (i == 0){
+                r_lin = rf / 12.92; 
+            } else if (i == 1) {
+                g_lin = gf / 12.92;
+            } else {
+                b_lin = bf / 12.92;
+            }
+        } else {
+            if (i == 0){
+                r_lin = ((rf + 0.055) / 1.055);
+                r_lin = std::pow(r_lin , 2.4); 
+            } else if (i == 1) {
+                g_lin = ((gf + 0.055) / 1.055);
+                g_lin = std::pow(g_lin , 2.4); 
+            } else {
+                b_lin = ((bf + 0.055) / 1.055);
+                b_lin = std::pow(b_lin , 2.4); 
+            }
+        }
+    }
+
+    std::cout << "( " << r_lin << " " << g_lin << " " << b_lin << " " << " )" << std::endl;
+
+    //Calculate Linear Luminance For That Pixel!!!
+    double linear_luminance = 0.2126 * r_lin + 0.7152 * g_lin + 0.0722 * b_lin;
+
+    std::cout << linear_luminance << std::endl;
+
 }   
